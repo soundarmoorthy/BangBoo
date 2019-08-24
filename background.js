5e43ade7bddd87cb40ac83c2dce381c2c7127335
Soundararajan Dhakshinamoorthy <dakshins@advisory.com>
2019-08-25 02:04:38 +0530

    Add template code to fetch pages, parse links and download files. This doesn't have actual implementation. But this should be the overall logic to download files

--- background.js
+++ background.js
@@ -1,10 +1,49 @@
 chrome.runtime.onMessage.addListener(
-    function(payload, sender, sendResponse)
-    {
-	var downloadUrl = payload.message;
-        chrome.downloads.download({"url" : downloadUrl});
-    }
+  function(payload, sender, sendResponse) {
+    var url = payload.message;
+    download(url);
+  }
 );
 
-function sendResponse() { 
+function sendResponse() {};
+
+function download(url) {
+  if (file(url)) {
+    chrome.downloads.download({
+      "url": url
+    });
+  } else {
+    links = fetchLinks(url);
+    downloadRecursive(url);
+  }
+};
+
+function file(url) {
+  success = false;
+  fetch(url)
+    .then(function(response) {
+      if (response.status == 200) {
+        html = $.parseHTML(response.text());
+        success = html != null;
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+      success = false;
+    })
+  return success;
+};
+
+function fetchLinks(url) {
+  var html = $.parseHTML(url);
+};
+
+function contents(url) {
+  fetch(url)
+    .then(function(response) {
+      //return content body here;
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
 }
